25a61568a6e819ec2c3c06528edffcf41748d40e
giolga <giolga.kumi@gmail.com>
2024-09-04 14:05:07 +0400

    forms and validations

--- FirstBlazor/obj/Debug/net8.0/FirstBlazor.AssemblyInfo.cs
+++ FirstBlazor/obj/Debug/net8.0/FirstBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10850da06b622bfb260e06238b15a72bc2838fa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
